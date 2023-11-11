16f1171d810442a98bb1627af480030b8e0a3f03
BuildTools <unconfigured@null.spigotmc.org>
2023-11-11 21:40:19 +0800

    2023/11/11

--- pages/index.vue
+++ pages/index.vue
@@ -3,11 +3,14 @@
 </script>
 
 <template>
-  <div class="dark:bg-black flex h-screen w-full flex-col items-center justify-center bg-white">
+<!--  <div class="dark:bg-black flex h-screen w-full flex-col items-center justify-center bg-white">-->
+<!--    <h1 class="text-6xl dark:text-white font-bold text-sky-400">2023</h1>-->
+<!--    <p class="text-8xl md:text-9xl dark:text-white font-bold text-gray-600">台東一支會</p>-->
+<!--  </div>-->
+  <div class="flex w-full flex-col items-center justify-center bg-black">
     <h1 class="text-6xl dark:text-white font-bold text-sky-400">2023</h1>
     <p class="text-8xl md:text-9xl dark:text-white font-bold text-gray-600">台東一支會</p>
   </div>
-
 </template>
 
 <style scoped>
