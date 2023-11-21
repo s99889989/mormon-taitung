a788875ffb6c489b02486cf3548deeace77b2de6
BuildTools <unconfigured@null.spigotmc.org>
2023-11-21 19:36:56 +0800

    2023/11/21

--- pages/admin/member/add.vue
+++ pages/admin/member/add.vue
@@ -11,6 +11,7 @@ onMounted( () => {
     english_name: '',
     gender: '男',
     birthday: '',
+    spouse: '',
     priesthood: '無聖職職位',
     calling: '',
     stake: '花蓮支聯會',
@@ -184,6 +185,15 @@ const getMemberName = (uuid) => {
           </select>
           <div style="height: 50px" class="ps-5 flex items-center grow bg-gray-50 border border-gray-300 text-gray-900 text-2xl rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
             {{getMemberName(membersStore.data.editData.mother)}}</div>
+        </div>
+
+        <div id="date" class="flex items-center">
+          <label for="address" style="width: 80px" class="text-3xl block font-medium text-gray-900 dark:text-white">配偶</label>
+          <select v-model="membersStore.data.editData.spouse" class="me-5 text-xl bg-gray-50 border grow border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <option :value="member.id" v-for="(member) in membersStore.memberList">{{member.name}}</option>
+          </select>
+          <div style="height: 50px" class="ps-5 flex items-center grow bg-gray-50 border border-gray-300 text-gray-900 text-2xl rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            {{getMemberName(membersStore.data.editData.spouse)}}</div>
         </div>
 
         <div class=" bg-gray-50 border border-gray-300 text-gray-900 text-2xl rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
--- pages/admin/member/edit.vue
+++ pages/admin/member/edit.vue
@@ -149,6 +149,8 @@ const getMemberName = (uuid) => {
           </select>
         </div>
 
+
+
         <div id="date" class="flex items-center">
           <label for="address" style="width: 80px" class="text-3xl block font-medium text-gray-900 dark:text-white">父親</label>
           <select v-model="membersStore.data.editData.father" class="me-5 text-xl bg-gray-50 border grow border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
@@ -165,6 +167,15 @@ const getMemberName = (uuid) => {
           </select>
           <div style="height: 50px" class="ps-5 flex items-center grow bg-gray-50 border border-gray-300 text-gray-900 text-2xl rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
             {{getMemberName(membersStore.data.editData.mother)}}</div>
+        </div>
+
+        <div id="date" class="flex items-center">
+          <label for="address" style="width: 80px" class="text-3xl block font-medium text-gray-900 dark:text-white">配偶</label>
+          <select v-model="membersStore.data.editData.spouse" class="me-5 text-xl bg-gray-50 border grow border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <option :value="member.id" v-for="(member) in membersStore.memberList">{{member.name}}</option>
+          </select>
+          <div style="height: 50px" class="ps-5 flex items-center grow bg-gray-50 border border-gray-300 text-gray-900 text-2xl rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            {{getMemberName(membersStore.data.editData.spouse)}}</div>
         </div>
 
         <div class=" bg-gray-50 border border-gray-300 text-gray-900 text-2xl rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
