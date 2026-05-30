947a956a4c572ef3c324c9d1ea477dc3ef6743f7
s99889989 <s02048898067@gmail.com>
2026-05-30 17:39:26 +0800

    2026/05/30-17:39

--- utils/api.js
+++ utils/api.js
@@ -2,7 +2,7 @@
 //http://localhost:8080/
 //https://madustrialtd.asuscomm.com:8080/
 // https://api.karltw.com/
-export const API_BASE_URL = 'https://api.karltw.com:8080/'
+export const API_BASE_URL = 'https://madustrialtd.asuscomm.com:8080/'
 
 export const GOOGLE_CLIENT_ID = '441605672654-9j73r51g6j2mar17ptblhskfvard1em9.apps.googleusercontent.com'
 
